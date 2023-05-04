cf131284f8e42f2c40fd013becfa034e34d6ec01
d.utiralov <d>
2023-05-04 20:17:49 +0300

    add dbt commands

---
+++ 02_orchestration_prefect/flows/dbt_commands.py
@@ -0,0 +1,33 @@
+from prefect import flow
+from prefect_dbt.cli.commands import DbtCoreOperation
+import argparse
+
+
+@flow
+def trigger_dbt_debug(target: str) -> str:
+    result = DbtCoreOperation(
+        commands=[f"dbt debug --target {target}"],
+        project_dir="04_dbt",
+        profiles_dir="."
+    ).run()
+    return result
+
+@flow
+def trigger_dbt_clean(target: str) -> str:
+    result = DbtCoreOperation(
+        commands=[f"dbt clean --target {target}"],
+        project_dir="04_dbt",
+        profiles_dir=".",
+    ).run()
+    return result
+
+@flow
+def trigger_dbt_incremental_run(target: str) -> str:
+    dbt_run = DbtCoreOperation(
+        commands=[f"dbt --no-partial-parse run --target {target}"],
+        project_dir="04_dbt",
+        profiles_dir=".",
+    ).trigger()
+    dbt_run.wait_for_completion()
+    dbt_output = dbt_run.fetch_result()
+    return dbt_output
